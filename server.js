620939cc93b762dfcb472abb39c88e5c91d0f089
Heavenly-Demonic <siddhant09876543210@gmail.com>
2025-02-16 19:16:29 +0530

    suicide

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const http=require('http');
+const socketIo=require('socket.io');
+
+const app=express();
+
+const server=http.createServer(app);
+
+const io=socketIo(server,{
+    cors:{
+        origin:"*",
+        methods:['GET','POST'],
+    },
+});
+
+const PORT=process.env.PORT||5000;
+io.on('connection',(socket)=>{
+    console.log("User connected");
+
+    socket.on('chat message',(msg)=>{
+        io.emit('chat message',msg);
+    });
+
+    socket.on('disconnect',()=>{
+        console.log('User disconnected');
+    });
+});
+server.listen(PORT,()=> console.log(`Server running on ${PORT}.`));
