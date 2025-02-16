49a8de642b8d6eb0c3b33674385168c32f6bc130
Heavenly-Demonic <siddhant09876543210@gmail.com>
2025-02-16 21:17:22 +0530

    suicide

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http=require('http');
 const socketIo=require('socket.io');
 const cors=require('cors');
 
-app.use(cors({origin:"https://open-chats.onrender.com"}))
+app.use(cors({origin:"https://open-chats.onrender.com"}));
 const app=express();
 
 const server=http.createServer(app);
