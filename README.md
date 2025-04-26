c23fa596444b3527cdfae9f368b1fece45e8d692
Heavenly-Demonic <siddhant09876543210@gmail.com>
2025-04-26 21:27:03 +0530

    suicide

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Simple chat App with NO-DATABASE .
+# Simple Chat app.
 
+#### A simple chat app where you can talk to anybody live at that instance.
+
+#### Basically its just a web-socket connected to a minimal chat UI made in React.
+
+#### Also I didn't use any database to store the chats, sessions.
+
+### Features:
+<li>Only 1 Session for all for everyone online.
+<li>You can talk to anybody those who are online at that time.
+<li>There's only one temporary UserId is created to separate your messages from others.
+<li>Just a refresh will delete all the chats on your device.
+
+#### If still wanna try goto the below link:
+
+https://open-chats.onrender.com
+
+<li> Host Server takes some time to start so wait for 10-20 seconds. 
--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import React,{useState,useEffect} from 'react';
 import io from 'socket.io-client';
 
-const backendAPI="https://open-chat-nqse.onrender.com";
+const backendAPI="http://localhost:5000";
+//const backendAPI="https://open-chat-nqse.onrender.com";
 const sessionId=Math.random().toString(36).substring(2,9);
 const App=()=>{
     const [socket,setSocket]=useState(null);
@@ -33,52 +34,53 @@ const App=()=>{
     };
 
     return (
-      <div className="app" style={{margin:'0 auto',width:'90%',textAlign:'center'}}>
-        <h1>OpenChat</h1>
-        <div className="info-block">
-        <button className="info">i</button>
-        <div className="tip">
-          <h3>Just a simple chat app project.</h3>
-          <h4>Things to know before use!</h4>
-          <li>This project have no database so the chats are not saved anywhere. Just a refresh will delete every message on your side.</li>
-          <li>Also it is OpenChat so if you send a message then it is visible to everyone who are live at that instance.</li>
-          <li>Good thing is you can talk to anyone from anywhere anonymously.<br></br>(!! Area69  will still know !!)</li>
-          <li>You can also say it an Universal Comment Section.</li>
-          <h4>Enjoy your talk if you find someone!</h4>
-        </div>
-        </div>
-        <div className="chat-container">
-          {chat.map((msg,index)=>(
-            <div className="msg-container" key={index} style={{ textAlign:msg.senderId===sessionId? 'left':'right',}}>
-              {msg.type==='code'?
-              (<div className="codesnip" style={{
-                justifyContent:msg.senderId===sessionId? 'left':'right',
-                borderEndEndRadius:msg.senderId===sessionId? '15px':'0px',
-                borderEndStartRadius:msg.senderId===sessionId?'0px':'15px',
-                }}>
-                  {msg.content}
-                </div>)
-              :(<div className="message" style={{
-                justifySelf:msg.senderId===sessionId? 'left':'right',
-                borderEndEndRadius:msg.senderId===sessionId? '20px':'0px',
-                borderEndStartRadius:msg.senderId===sessionId?'0px':'20px'}}>
-                  {msg.content}
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-        <form onSubmit={sendMessage}>
-          <textarea
-            className="txt-box"
-            rows="3"
-            placeholder="Type your message here ..."
-            value={message}
-            onChange={(e)=>setMessage(e.target.value)}
-          ></textarea>
-          <button type="submit" className="send-btn">Send</button><br></br>
-        </form>
-      </div>
+								<div className="app" style={{margin:'0 auto',width:'90%',textAlign:'center'}}>
+												<h1>OpenChat</h1>
+												<div className="info-block">
+																<button className="info">i</button>
+																<div className="tip">
+																				<h3>Just a simple chat app project.</h3>
+																				<h4>Note :</h4>
+																				<ul>
+																								<li>This project have no database so the chats are not saved anywhere. Just a refresh will delete every message on your side.</li>
+																								<li>Also it is OpenChat so if you send a message then it is visible to everyone who are live at that instance.</li>
+																								<li>Good thing is you can talk to anyone from anywhere anonymously.<br></br></li>
+																				</ul>
+																				<h4>Enjoy your talk if you find someone !!!</h4>
+																</div>
+												</div>
+												<div className="chat-container">
+																{chat.map((msg,index)=>(
+																				<div className="msg-container" key={index} style={{ textAlign:msg.senderId===sessionId? 'left':'right',}}>
+																								{msg.type==='code'?
+																									(<div className="codesnip" style={{
+																																			justifyContent:msg.senderId===sessionId? 'left':'right',
+																																			borderEndEndRadius:msg.senderId===sessionId? '15px':'0px',
+																																			borderEndStartRadius:msg.senderId===sessionId?'0px':'15px',
+																															}}>
+																														{msg.content}
+																										</div>)
+																									:(<div className="message" style={{
+																																				justifySelf:msg.senderId===sessionId? 'left':'right',
+																																				borderEndEndRadius:msg.senderId===sessionId? '20px':'0px',
+																																				borderEndStartRadius:msg.senderId===sessionId?'0px':'20px'}}>
+																															{msg.content}
+																											</div>
+																										)}
+																				</div>
+																))}
+												</div>
+												<form onSubmit={sendMessage}>
+																<textarea
+																				className="txt-box"
+																				rows="3"
+																				placeholder="Type your message here ..."
+																				value={message}
+																				onChange={(e)=>setMessage(e.target.value)}
+																></textarea>
+																<button type="submit" className="send-btn">Send</button><br></br>
+												</form>
+								</div>
     );
 };
 export default App;
